5202fd7c8f41ad1bfbe69ea09462e03e6136404a
jessie5557 <ychen38@chicagobooth.edu>
2025-02-05 07:32:08 +0000

    done

--- app.rb
+++ app.rb
@@ -27,16 +27,21 @@ get("/:from_currency") do
 
   @original_currency = params.fetch("from_currency")
 
-
   erb(:convert_a)
 end
 
 
 get("/:from_currency/:to_currency") do
+
   @original_currency = params.fetch("from_currency")
   @destination_currency = params.fetch("to_currency")
 
-  @convert_url = "https://api.exchangerate.host/list?access_key=#{ ENV.fetch("FX_KEY")}&from=#{@original_currency}&to=#{@destination_currency}&amount=1"
+  @convert_url = "https://api.exchangerate.host/convert?from=#{@original_currency}&to=#{@destination_currency}&amount=1&access_key=#{ENV.fetch("FX_KEY")}"
+  @raw_response = HTTP.get(@convert_url)
+  @string_response = @raw_response.to_s
+  @parsed_response = JSON.parse(@string_response)
+  @result = @parsed_response.fetch("result").to_s
+
 
   erb(:result)
 end
